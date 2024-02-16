10ff6f0c33a07c89404bf386fca021ed66937ca7
MikkoAyaka <3401286177@qq.com>
2024-02-16 21:14:17 +0800

    修正了任务书名称无法正常识别的bug

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/resource/item/TaskBook.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/resource/item/TaskBook.kt
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 import org.wolflink.minecraft.plugin.eclipticengineering.Quality
 import org.wolflink.minecraft.plugin.eclipticengineering.SpecialItemType
 import org.wolflink.minecraft.plugin.eclipticengineering.config.MESSAGE_PREFIX
@@ -31,6 +32,7 @@ object TaskBook: Listener {
         player.sendActionBar("$MESSAGE_PREFIX 你拿到了任务书".toComponent())
         player.playSound(player, Sound.ENTITY_VILLAGER_AMBIENT,1f,1.5f)
     }
+    private var lastUpdateMeta: ItemMeta = defaultItem.itemMeta
     private fun update(player: Player,bookItem: ItemStack) {
         val goal = GoalHolder.nowGoal ?: return
         val newMeta = Material.BOOK.createSpecialItem(
@@ -39,18 +41,19 @@ object TaskBook: Listener {
             "任务书",
             true,
             goal.finishConditions.map {
-                if(it.check()) "    <green>☑ ${SECONDARY_TEXT_COLOR}<italic>${it.description}</italic>"
-                else "    <white>☐ ${PRIMARY_TEXT_COLOR}${it.description}"
+                if(it.check()) "<green>☑ ${SECONDARY_TEXT_COLOR}<italic>${it.description}</italic>"
+                else "<white>☐ ${PRIMARY_TEXT_COLOR}${it.description}"
             }
         ).itemMeta
         bookItem.itemMeta = newMeta
+        lastUpdateMeta = newMeta
 
         player.sendActionBar("$MESSAGE_PREFIX 任务书已刷新".toComponent())
         player.playSound(player, Sound.ENTITY_VILLAGER_AMBIENT,1f,1.5f)
     }
     @EventHandler
     fun onInteract(e: PlayerInteractEvent) {
-        if(e.action.isRightClick && e.item?.displayName() == defaultItem.displayName()) {
+        if(e.action.isRightClick && e.item?.itemMeta == lastUpdateMeta) {
             update(e.player,e.item!!)
         }
     }
