45a5287e8c0de8a0290c4320532a354c22b2f17a
MikkoAyaka <3401286177@qq.com>
2024-02-22 14:48:45 +0800

    修正了建筑异常损失耐久的bug

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/structure/special/EnergySource.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/structure/special/EnergySource.kt
@@ -1,24 +1,15 @@
 package org.wolflink.minecraft.plugin.eclipticengineering.structure.special
 
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
-import org.wolflink.minecraft.plugin.eclipticengineering.EEngineeringScope
 import org.wolflink.minecraft.plugin.eclipticengineering.blueprint.ConditionBlueprint
 import org.wolflink.minecraft.plugin.eclipticengineering.dictionary.VirtualResourceType
 import org.wolflink.minecraft.plugin.eclipticengineering.requirement.ItemRequirement
 import org.wolflink.minecraft.plugin.eclipticengineering.requirement.VirtualRequirement
 import org.wolflink.minecraft.plugin.eclipticengineering.structure.api.GameStructure
 import org.wolflink.minecraft.plugin.eclipticengineering.structure.api.GameStructureTag
-import org.wolflink.minecraft.plugin.eclipticstructure.event.structure.StructureAvailableEvent
-import org.wolflink.minecraft.plugin.eclipticstructure.repository.StructureRepository
-import org.wolflink.minecraft.plugin.eclipticstructure.repository.StructureZoneRelationRepository
-import org.wolflink.minecraft.plugin.eclipticstructure.repository.ZoneRepository
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.IStructureListener
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.StructureCompanion
-import org.wolflink.minecraft.plugin.eclipticstructure.structure.Zone
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.blueprint.Blueprint
 import org.wolflink.minecraft.plugin.eclipticstructure.structure.builder.Builder
 
@@ -31,8 +22,6 @@ class EnergySource private constructor(blueprint: Blueprint, builder: Builder) :
     override val customListeners by lazy { listOf(this) }
 
     companion object : StructureCompanion<EnergySource>() {
-        // 影响半径(格)
-        private const val EFFECT_RADIUS = 35
         private const val STRUCTURE_NAME = "幽光能量发生场"
         override fun supplier(blueprint: Blueprint, builder: Builder): EnergySource {
             return EnergySource(blueprint, builder)
@@ -50,46 +39,5 @@ class EnergySource private constructor(blueprint: Blueprint, builder: Builder) :
                 VirtualRequirement("需要 40 木材", VirtualResourceType.WOOD, 40)
             )
         )
-    }
-
-    /**
-     * 影响其它建筑结构
-     */
-    private suspend fun effectStructures() {
-        val center = builder.buildLocation
-        val effectZone = Zone(
-            center.world.name,
-            center.blockX - EFFECT_RADIUS..center.blockX + EFFECT_RADIUS,
-            center.blockY - EFFECT_RADIUS..center.blockY + EFFECT_RADIUS,
-            center.blockZ - EFFECT_RADIUS..center.blockZ + EFFECT_RADIUS
-        )
-        while (true) {
-            // 能源中心生效时对范围内建筑供能
-            if (available) {
-                // 在影响范围内的建筑结构
-                val effectedStructures = ZoneRepository.findByOverlap(effectZone)
-                    .mapNotNull(StructureZoneRelationRepository::find1)
-                    .toSet()
-                // 不在影响范围内的建筑结构
-                val notEffectedStructures = StructureRepository.findAll() - effectedStructures
-                notEffectedStructures.forEach {
-                    it.doDamage(5 + (0.03 * (blueprint.maxDurability - durability)), DamageSource.LACK_OF_ENERGY, this)
-                }
-            }
-            // 能源中心失效，所有建筑开始失去耐久
-            else {
-                StructureRepository.findAll().forEach {
-                    it.doDamage(5 + (0.03 * (blueprint.maxDurability - durability)), DamageSource.LACK_OF_ENERGY, this)
-                }
-            }
-
-            // 每秒检查一次
-            delay(1000)
-        }
-    }
-
-    private var effectJob: Job? = null
-    override fun onAvailable(e: StructureAvailableEvent) {
-        if (effectJob == null || effectJob!!.isCompleted) effectJob = EEngineeringScope.launch { effectStructures() }
     }
 }
