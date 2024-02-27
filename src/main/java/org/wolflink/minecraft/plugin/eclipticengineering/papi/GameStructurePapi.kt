817eb3462d283c739f02724b7d3ec747592dc1e6
MikkoAyaka <3401286177@qq.com>
2024-02-27 21:32:38 +0800

    调整 GameStructure 变量

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/papi/GameStructurePapi.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/papi/GameStructurePapi.kt
@@ -8,7 +8,7 @@ import org.wolflink.minecraft.plugin.eclipticengineering.dictionary.StructureTyp
 import org.wolflink.minecraft.plugin.eclipticengineering.utils.ReflectionAPI
 
 /**
- * %eegs_xxxxx_x_conditions% 类型名称为 xxxxx 等级为 x 的建筑的建造条件(如果有下划线，用连字符代替-)
+ * %eegs_xxxxx_x_buildConditions% 类型名称为 xxxxx 等级为 x 的建筑的建造条件(如果有下划线，用连字符代替-)
  * %eegs_xxxxx_tags% 建筑类型为 xxxxx 的标签
  * %eegs_xxxxx_x_tags% 建筑类型为 xxxxx 的标签
  * %eegs_xxxxx_x_blueprint_xxx% 获取 xxxxx 建筑 x 等级的蓝图中的 xxx 属性
@@ -20,7 +20,8 @@ object GameStructurePapi: PlaceholderExpansion()  {
 
     override fun getVersion() = "1.0.0"
 
-    private val regexConditions = "[A-Za-z-]+_\\d+_conditions".toRegex()
+    private val regexBuildConditions = "[A-Za-z-]+_\\d+_buildConditions".toRegex()
+    private val regexRepairConditions = "[A-Za-z-]+_\\d+_repairConditions".toRegex()
     private val regexTags1 = "[A-Za-z-]+_tags".toRegex()
     private val regexTags2 = "[A-Za-z-]+_\\d+_tags".toRegex()
     private val regexBlueprint = "[A-Za-z-]+_\\d+_blueprint_[A-Za-z]+".toRegex()
@@ -29,12 +30,22 @@ object GameStructurePapi: PlaceholderExpansion()  {
         val args = params.split('_')
         try {
             when {
-                params.matches(regexConditions) -> {
+                params.matches(regexBuildConditions) -> {
                     val structureType = StructureType.valueOf(args[0].uppercase().replace('-','_'))
                     val level = args[1].toInt()
                     val blueprint = structureType.blueprints[level-1] as ConditionBlueprint
                     if(blueprint.buildConditions.isEmpty()) return "    &#E8E8E8无"
                     return blueprint.buildConditions.joinToString(separator = "\n") {
+                        if(it.isSatisfy(player.player)) "    &#33FF33☑"+it.description
+                        else "    &#FF3333☒"+it.description
+                    }
+                }
+                params.matches(regexRepairConditions) -> {
+                    val structureType = StructureType.valueOf(args[0].uppercase().replace('-','_'))
+                    val level = args[1].toInt()
+                    val blueprint = structureType.blueprints[level-1] as ConditionBlueprint
+                    if(blueprint.repairConditions.isEmpty()) return "    &#E8E8E8无"
+                    return blueprint.repairConditions.joinToString(separator = "\n") {
                         if(it.isSatisfy(player.player)) "    &#33FF33☑"+it.description
                         else "    &#FF3333☒"+it.description
                     }
