9ae96f66ac4344567569700ce34958f85fc71cbf
MikkoAyaka <3401286177@qq.com>
2024-02-27 21:48:15 +0800

    将部分颜色变量封装至 EclipticStructure

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/config/Lang.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/config/Lang.kt
@@ -1,6 +1,7 @@
 package org.wolflink.minecraft.plugin.eclipticengineering.config
 
-import org.wolflink.minecraft.plugin.eclipticengineering.dictionary.PRIMARY_TEXT_COLOR
-import org.wolflink.minecraft.plugin.eclipticengineering.dictionary.SPLITER_COLOR
+import org.wolflink.minecraft.plugin.eclipticstructure.config.PRIMARY_TEXT_COLOR
+import org.wolflink.minecraft.plugin.eclipticstructure.config.SPLITER_COLOR
 
-val MESSAGE_PREFIX = "<reset>$SPLITER_COLOR[ <gradient:#6421de:#f7af59>星辉工程</gradient> $SPLITER_COLOR]$PRIMARY_TEXT_COLOR"
+
+const val MESSAGE_PREFIX = "<reset>$SPLITER_COLOR[ <gradient:#6421de:#f7af59>星辉工程</gradient> $SPLITER_COLOR]$PRIMARY_TEXT_COLOR"
--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/dictionary/Dictionary.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/dictionary/Dictionary.kt
@@ -128,7 +128,3 @@ fun Material.isHoe() = this in HOE_MATERIAL
 fun Material.isAxe() = this in AXE_MATERIAL
 fun Material.isPickaxe() = this in PICKAXE_MATERIAL
 fun Material.isOre() = this in ORE_MATERIAL
-
-const val SPLITER_COLOR = "<#9C9C9C>"
-const val SECONDARY_TEXT_COLOR = "<#E8E8E8>"
-const val PRIMARY_TEXT_COLOR = "<#FFFAFA>"
