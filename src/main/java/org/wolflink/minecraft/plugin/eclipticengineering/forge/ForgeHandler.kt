412659e9ee93194d72785b1207566c493cdda297
MikkoAyaka <3401286177@qq.com>
2024-02-29 12:25:31 +0800

    为锻造添加了关于能力的检测

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/forge/ForgeHandler.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/forge/ForgeHandler.kt
@@ -3,11 +3,10 @@ package org.wolflink.minecraft.plugin.eclipticengineering.forge
 import org.bukkit.Bukkit
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+import org.wolflink.minecraft.plugin.eclipticengineering.ability.Ability
 import org.wolflink.minecraft.plugin.eclipticengineering.config.Config.forgeMenuCmd
 import org.wolflink.minecraft.plugin.eclipticengineering.config.MESSAGE_PREFIX
-import org.wolflink.minecraft.plugin.eclipticengineering.requirement.ItemRequirement
-import org.wolflink.minecraft.plugin.eclipticengineering.requirement.Requirement
-import org.wolflink.minecraft.plugin.eclipticengineering.requirement.SpecialItemRequirement
+import org.wolflink.minecraft.plugin.eclipticengineering.requirement.*
 import org.wolflink.minecraft.plugin.eclipticengineering.resource.item.SpecialDiamond
 import org.wolflink.minecraft.plugin.eclipticengineering.resource.item.SpecialGold
 import org.wolflink.minecraft.plugin.eclipticengineering.resource.item.SpecialIron
@@ -16,14 +15,16 @@ import org.wolflink.minecraft.plugin.eclipticstructure.extension.parsePapi
 import org.wolflink.minecraft.plugin.eclipticstructure.extension.toComponent
 
 object ForgeHandler {
-    val weaponConditions = setOf<Requirement>(
+    val weaponConditions = setOf(
         SpecialItemRequirement(SpecialIron,3),
         SpecialItemRequirement(SpecialGold,3),
         SpecialItemRequirement(SpecialDiamond,1),
+        AbilityCondition(Ability.SMELTING,2)
     )
-    val repairConditions = setOf<Requirement>(
+    val repairConditions = setOf<Condition>(
         SpecialItemRequirement(SpecialIron,2),
-        SpecialItemRequirement(SpecialGold,1)
+        SpecialItemRequirement(SpecialGold,1),
+        AbilityCondition(Ability.SMELTING,2)
     )
     private val randomWeaponCommand = mapOf(
         "itemgenerator give %player_name% 武器_普通" to 15,
@@ -46,22 +47,22 @@ object ForgeHandler {
     fun forgeWeapon(player: Player) {
         // 不满足条件
         if(weaponConditions.any { !it.isSatisfy(player) }) {
-            player.sendMessage("$MESSAGE_PREFIX 你缺少必要的锻造材料进行锻造。".toComponent())
+            player.sendMessage("$MESSAGE_PREFIX 你不满足锻造所需的条件。".toComponent())
             player.playSound(player, Sound.ENTITY_VILLAGER_NO,1f,1f)
             return
         }
-        weaponConditions.forEach { it.delivery(player) }
+        weaponConditions.forEach { if(it is Requirement) it.delivery(player) }
         player.sendMessage("$MESSAGE_PREFIX 锻造成功。".toComponent())
         player.playSound(player, Sound.ENTITY_VILLAGER_YES,1f,1f)
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), randomWeaponCommand.random().parsePapi(player))
     }
     fun forgeRepair(player: Player) {
         if(repairConditions.any{ !it.isSatisfy(player) }) {
-            player.sendMessage("$MESSAGE_PREFIX 你缺少必要的锻造材料进行锻造。".toComponent())
+            player.sendMessage("$MESSAGE_PREFIX 你不满足锻造所需的条件。".toComponent())
             player.playSound(player, Sound.ENTITY_VILLAGER_NO,1f,1f)
             return
         }
-        repairConditions.forEach { it.delivery(player) }
+        repairConditions.forEach { if(it is Requirement) it.delivery(player) }
         player.sendMessage("$MESSAGE_PREFIX 锻造成功。".toComponent())
         player.playSound(player, Sound.ENTITY_VILLAGER_YES,1f,1f)
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), randomRepairCommand.random().parsePapi(player))
