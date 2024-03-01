93b21916871b3b7f4a470f640ccca59c4e723b1a
MikkoAyaka <3401286177@qq.com>
2024-03-01 16:28:40 +0800

    补充各个职业的被动能力

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/ability/OccupationApplier.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/ability/OccupationApplier.kt
@@ -13,10 +13,24 @@ import org.wolflink.minecraft.plugin.eclipticengineering.extension.gamingPlayers
  */
 object OccupationApplier {
     fun init() {
-        EclipticEngineering.runTaskTimer(20,20) {
+        EclipticEngineering.runTaskTimer(20 * 3,20 * 3) {
+            // 战士
             gamingPlayers.filter { it.abilityTable.occupationType == OccupationType.WARRIOR }.forEach {
                 it.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION,20 * 5,0,false,false))
                 it.addPotionEffect(PotionEffect(PotionEffectType.INCREASE_DAMAGE,20 * 5,0,false,false))
+            }
+            // 建筑师
+            gamingPlayers.filter { it.abilityTable.occupationType == OccupationType.BUILDER }.forEach {
+                it.addPotionEffect(PotionEffect(PotionEffectType.GLOWING,20 * 5,0,false,false))
+            }
+            // 打工仔
+            gamingPlayers.filter { it.abilityTable.occupationType == OccupationType.MINER }.forEach {
+                it.addPotionEffect(PotionEffect(PotionEffectType.FAST_DIGGING,20 * 5,0,false,false))
+                it.addPotionEffect(PotionEffect(PotionEffectType.SPEED,20 * 5,0,false,false))
+            }
+            // 铁匠
+            gamingPlayers.filter { it.abilityTable.occupationType == OccupationType.BLACKSMITH }.forEach {
+                it.addPotionEffect(PotionEffect(PotionEffectType.HEALTH_BOOST,20 * 5,2,false,false))
             }
         }
     }
