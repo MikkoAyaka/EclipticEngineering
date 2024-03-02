df6d3b4d69dafe5655afd99f703d9f70ee843b72
MikkoAyaka <3401286177@qq.com>
2024-03-02 15:11:14 +0800

    呆在会议室也不会刷怪了

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/monster/strategy/NotSpawnStrategy.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/monster/strategy/NotSpawnStrategy.kt
@@ -3,13 +3,15 @@ package org.wolflink.minecraft.plugin.eclipticengineering.monster.strategy
 import org.bukkit.entity.Player
 import org.wolflink.minecraft.plugin.eclipticengineering.monster.SpawnerAttribute
 import org.wolflink.minecraft.plugin.eclipticengineering.structure.foothold.LivingHouse
+import org.wolflink.minecraft.plugin.eclipticengineering.structure.foothold.MeetingPlace
 import org.wolflink.minecraft.plugin.eclipticstructure.repository.StructureRepository
 
 class NotSpawnStrategy(spawnerAttribute: SpawnerAttribute) : SpawnStrategy(spawnerAttribute)  {
 
     override fun isApplicable(player: Player): Boolean {
-        val livingHouse = StructureRepository.findBy { it is LivingHouse }.firstOrNull() ?: return false
-        return player.location in livingHouse.zone
+        val livingHouse = StructureRepository.findBy { it is LivingHouse }.firstOrNull()
+        val meetingPlace = StructureRepository.findBy { it is MeetingPlace }.firstOrNull()
+        return livingHouse?.zone?.contains(player.location) == true || meetingPlace?.zone?.contains(player.location) == true
     }
 
     override fun spawn(player: Player, triedCount: Int) {
