f30955f67e8b6e2df1c4b9b1ef3fdc12f9534c9a
MikkoAyaka <3401286177@qq.com>
2024-03-03 11:10:59 +0800

    优化范围聊天效果，加大距离

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/roleplay/RangeChat.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/roleplay/RangeChat.kt
@@ -10,19 +10,21 @@ import org.wolflink.minecraft.plugin.eclipticengineering.utils.RandomAPI
 import org.wolflink.minecraft.plugin.eclipticstructure.extension.toComponent
 
 object RangeChat: Listener {
+    private const val CHAT_RANGE = 50
     @EventHandler(priority = EventPriority.HIGHEST)
     fun on(e: AsyncChatEvent) {
         e.isCancelled = true
         val chatter = e.player
         onlinePlayers
-            .filter { it.location.distance(chatter.location) <= 20 }
+            .filter { it.location.distance(chatter.location) <= CHAT_RANGE }
             .forEach {
-                if(it.hasLineOfSight(chatter) || it == chatter) {
+                if(it.hasLineOfSight(chatter) || it == chatter || it.location.distance(chatter.location) < CHAT_RANGE * 0.5) {
                     it.sendMessage(chatter.displayName().append(" <gray>说 <white>".toComponent()).append(e.message()))
                 } else {
+                    val msgLoss = it.location.distance(chatter.location) / CHAT_RANGE - 0.5
                     val newMessageChars = PlainTextComponentSerializer.plainText().serialize(e.message()).toCharArray()
                     for (i in newMessageChars.indices) {
-                        if(RandomAPI.nextDouble() < 0.5) newMessageChars[i] = '?'
+                        if(RandomAPI.nextDouble() < msgLoss) newMessageChars[i] = '?'
                     }
                     it.sendMessage("<yellow>???</yellow> <gray>说 <white>".toComponent().append(newMessageChars.concatToString().toComponent()))
                 }
