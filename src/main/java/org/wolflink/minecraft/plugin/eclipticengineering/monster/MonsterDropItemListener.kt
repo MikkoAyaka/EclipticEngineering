355871feb9ed726ad5508af8a908441af6d62e86
MikkoAyaka <3401286177@qq.com>
2024-03-05 20:46:57 +0800

    怪物不再掉落下届合金碎片

--- src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/monster/MonsterDropItemListener.kt
+++ src/main/java/org/wolflink/minecraft/plugin/eclipticengineering/monster/MonsterDropItemListener.kt
@@ -8,20 +8,24 @@ import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.inventory.ItemStack
 import org.wolflink.minecraft.plugin.eclipticengineering.utils.RandomAPI
 import org.wolflink.minecraft.plugin.eclipticengineering.utils.toRandomDistribution
+import java.util.Random
 
 object MonsterDropItemListener: Listener {
+    private val random = Random()
     private const val DROP_CHANCE = 0.4
     private val dropTable = mapOf(
+        ItemStack(Material.IRON_NUGGET,4) to 5,
         ItemStack(Material.IRON_INGOT) to 20,
-        ItemStack(Material.GOLD_INGOT) to 7,
+        ItemStack(Material.GOLD_INGOT) to 10,
+        ItemStack(Material.LAPIS_LAZULI,3) to 5,
         ItemStack(Material.DIAMOND) to 3,
-        ItemStack(Material.NETHERITE_SCRAP) to 2,
-        ItemStack(Material.WITHER_SKELETON_SKULL) to 1
+//        ItemStack(Material.NETHERITE_SCRAP) to 2,
+//        ItemStack(Material.WITHER_SKELETON_SKULL) to 1
     ).toRandomDistribution()
     @EventHandler(ignoreCancelled = true)
     fun on(e: EntityDeathEvent) {
         // 死亡的是怪物
-        if(e.entity is Monster && RandomAPI.nextDouble() <= DROP_CHANCE) {
+        if(e.entity is Monster && random.nextDouble() <= DROP_CHANCE) {
             // 掉落资源
             e.entity.world.dropItemNaturally(e.entity.location, dropTable.random())
         }
